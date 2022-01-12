55b83bdfd0f28155e7f8bb1e1b9fd983f3f30777
yuuinchii <yuichi.ikeda@zozo.com>
2022-01-12 17:21:19 +0900

    p15

--- src/test/java/tdd/theMoneyExample/MoneyTest.java
+++ src/test/java/tdd/theMoneyExample/MoneyTest.java
@@ -8,9 +8,11 @@ public class MoneyTest {
   @Test
   public void Multiplication(){
       Dollar five = new Dollar(5);
+      Dollar product = five.times(2);
+      assertEquals(10, product.amount);
 
-      five.times(2);
-      assertEquals(10, five.amount);
+      product = five.times(3);
+      assertEquals(15, product.amount);
   }
 
 }
