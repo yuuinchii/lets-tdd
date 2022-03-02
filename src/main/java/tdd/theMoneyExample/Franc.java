bf5864875d9047ba1be5d12328329727c7a91f77
yuuinchii <yuichi.ikeda@zozo.com>
2022-03-02 17:13:55 +0900

    p34

---
+++ src/main/java/tdd/theMoneyExample/Franc.java
@@ -0,0 +1,18 @@
+package tdd.theMoneyExample;
+
+class Dollar {
+  private int amount;
+  Dollar(int amount) {
+    this.amount = amount;
+  }
+
+  Dollar times(int multiplier)  {
+    return new Dollar(amount * multiplier);
+  }
+
+  public  boolean equals(Object object){
+    Dollar dollar = (Dollar) object;
+    return amount == dollar.amount;
+  }
+
+}
--- src/test/java/tdd/theMoneyExample/MoneyTest.java
+++ src/test/java/tdd/theMoneyExample/MoneyTest.java
@@ -6,7 +6,7 @@ import static org.junit.jupiter.api.Assertions.*;
 public class MoneyTest {
 
   @Test
-  public void Multiplication(){
+  public void testMultiplication(){
       Dollar five = new Dollar(5);
       assertEquals(new Dollar(10), five.times(2));
       assertEquals(new Dollar(10), five.times(3));
@@ -16,6 +16,13 @@ public class MoneyTest {
   public void testEquality(){
     assertTrue(new Dollar(5).equals(new Dollar(5)));
     assertFalse(new Dollar(5).equals(new Dollar(6)));
+  }
+
+  @Test
+  public void testFrancMultiplication(){
+    Dollar five = new Dollar(5);
+    assertEquals(new Dollar(10), five.times(2));
+    assertEquals(new Dollar(10), five.times(3));
   }
 
 }
